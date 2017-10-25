0b38189e61997e9eb9693d61ad438ad70eb90ba8
SuzanneMcC <smccarthy13@gmail.com>
2017-10-25 12:51:54 +0100

    Create INSTALL_SOL_SERVER.bat

---
+++ INSTALL_SOL_SERVER.bat
@@ -0,0 +1,4 @@
+echo Installing SOL SERVER
+git clone https://SuzanneMcC:123piggy@github.com/SuzanneMcC/DATAS_SOL_SERVER.git
+cd DATAS_SOL_SERVER
+start "Installing SOL SERVER" npm install --save ^&^& exit
