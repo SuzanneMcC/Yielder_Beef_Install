3aac37caa84a496b54618ba0a98da41e64c81772
SuzanneMcC <smccarthy13@gmail.com>
2017-10-25 12:50:22 +0100

    Create INSTALL_EOL_SERVER.bat

---
+++ INSTALL_EOL_SERVER.bat
@@ -0,0 +1,4 @@
+echo Installing EOL SERVER
+git clone "https://SuzanneMcC:123piggy@github.com/SuzanneMcC/DATAS_EOL_SERVER.git"
+cd DATAS_EOL_SERVER
+start "Installing EOL SERVER" npm install --save && exit
