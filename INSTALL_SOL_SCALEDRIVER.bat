14f1355595352eb280f8509b9f21d7e81fd705cb
SuzanneMcC <smccarthy13@gmail.com>
2017-10-25 12:51:03 +0100

    Create INSTALL_SOL_SCALEDRIVER.bat

---
+++ INSTALL_SOL_SCALEDRIVER.bat
@@ -0,0 +1,4 @@
+echo Installing SOL ScaleDriver
+git clone https://SuzanneMcC:123piggy@github.com/SuzanneMcC/DATAS_Kepak_SOL_ScaleDriver.git
+cd DATAS_Kepak_SOL_ScaleDriver
+start "Installing SOL ScaleDriver" npm install --save ^&^& exit
