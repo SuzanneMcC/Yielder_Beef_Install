51b42643ddd80fc65aba4904181c47230debe3d3
SuzanneMcC <smccarthy13@gmail.com>
2017-10-25 12:49:44 +0100

    Create INSTALL_EOL_SCALEDRIVER.bat

---
+++ INSTALL_EOL_SCALEDRIVER.bat
@@ -0,0 +1,4 @@
+echo Downloading EOL ScaleDriver
+git clone "https://SuzanneMcC:123piggy@github.com/SuzanneMcC/DATAS_Kepak_EOL_ScaleDriver.git"
+cd DATAS_Kepak_EOL_ScaleDriver
+start "Installing EOL ScaleDriver" npm install --save && exit
