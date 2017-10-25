d0287a6b30f48c018bb2c8ed28e7d09ce1ef61ac
SuzanneMcC <smccarthy13@gmail.com>
2017-10-25 12:20:47 +0100

    andrew4

---
+++ Delete_sql.sql
@@ -0,0 +1,16 @@
+SET SQL_SAFE_UPDATES = 0;
+delete FROM yielderkpkcl.tblweightssol;
+SELECT * FROM yielderkpkcl.tblweightssol;
+
+delete FROM yielderkpkcl.tblweightseol;
+SELECT * FROM yielderkpkcl.tblweightseol;
+
+delete FROM yielderkpkcl.tbloutput;
+SELECT * FROM yielderkpkcl.tbloutput;
+
+delete FROM yielderkpkcl.tblproductyields;
+
+SELECT * FROM yielderkpkcl.tblproductyields;
+
+delete FROM yielderkpkcl.tblbatch;
+SELECT * FROM yielderkpkcl.tblbatch;
